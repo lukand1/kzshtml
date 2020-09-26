0cafe773df1fec85faec2d929f2686ec2cc24aee
lukand1 <37643964+lukand1@users.noreply.github.com>
2020-09-26 20:12:05 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,45 @@
+const submitButton = document.getElementById("submitButton");
+const feedbackButton = document.getElementById("feedbackButton");
+const newsletterFieldName = document.getElementById("newsletterFieldName");
+const newsletterFieldEmail = document.getElementById("newsletterFieldEmail");
+const feedbackArea = document.getElementById("feedbackArea");
+const errorFeedback = document.getElementById("errorFeedback");
+
+function saveToLocalStorage() {
+  const newsletterValueName = newsletterFieldName.value;
+  const newsletterValueEmail = newsletterFieldEmail.value;
+
+  if (newsletterValueName.length > 0 && newsletterValueEmail.length > 0) {
+    localStorage.setItem("newsletterValueName", newsletterValueName);
+    localStorage.setItem("newsletterValueEmail", newsletterValueEmail);
+
+    alert("Submit succesful");
+  }
+}
+
+function saveFeedbackToLS(value) {
+  localStorage.setItem("feedback", value);
+}
+
+function submitFeedback() {
+  errorFeedback.style.display = "none";
+  var feedbackValue = document.forms["feedbackForm"]["feedbackText"].value;
+  if (feedbackValue == "" || feedbackValue == null) {
+    errorFeedback.style.display = "block";
+    return false;``
+  } else {
+    saveFeedbackToLS(feedbackValue);
+    alert("Feedback submitted succesfully");
+  }
+}
+
+feedbackArea.addEventListener("keypress", (e) => {
+  if (e.key === "Enter") {
+    e.preventDefault();
+    submitFeedback();
+  }
+});
+
+feedbackButton.addEventListener("click", () => {
+  submitFeedback();
+});
